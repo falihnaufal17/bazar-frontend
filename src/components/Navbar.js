3ac7b89fed61758234a3c4b3087f87aad74e7764
muhammad yusuf <ghinazahra56@gmail.com>
2020-11-05 16:52:21 +0700

    modal login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,12 @@ import SearchIcon from '../assets/icons/SEARCH.svg';
 import SignInIcon from '../assets/icons/SIGN IN.svg';
 import BasketIcon from '../assets/icons/BASKET.svg';
 import Menu from '../assets/icons/MENU.svg';
+import Facebook from '../assets/icons/FACEBOOK.svg';
+import Google from '../assets/icons/GOOGLE.svg';
 
 const Navbar = (props) => {
     return(
+        <>
         <nav className="navbar navbar-expand-lg">
             <div className="container">
                 <div className="row align-items-center">
@@ -21,7 +24,7 @@ const Navbar = (props) => {
                     <div className="col-auto order-4 order-md-4 order-lg-4">
                         <div className="valign">
                             <div className="inner">
-                                <a href="#" className="btn-login">
+                                <a href="#" className="btn-login" data-toggle="modal" data-target="#modalLogin">
                                     <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fillRule="evenodd" clipRule="evenodd" d="M8.38098 15H13.5H16V12.5V2.5V0H13.5H8.38098V2.5H13.5V12.5H8.38098V15Z" fill="black"/>
                                         <path d="M8.91128 8.38775C9.20417 8.09486 9.20417 7.61999 8.91128 7.32709L4.13831 2.55412C3.84542 2.26123 3.37054 2.26123 3.07765 2.55412C2.78476 2.84701 2.78476 3.32189 3.07765 3.61478L7.32029 7.85742L3.07765 12.1001C2.78476 12.393 2.78476 12.8678 3.07765 13.1607C3.37054 13.4536 3.84542 13.4536 4.13831 13.1607L8.91128 8.38775ZM0 8.60742H8.38095V7.10742H0V8.60742Z" fill="black"/>
@@ -259,43 +262,54 @@ const Navbar = (props) => {
                                 </div>
                             </div>
                         </li>
-                        {/* <li className="nav-item">
-                            <a className="nav-link" href="/products/women">Women</a>
-                            {props.match.params.menu == 'women' ? (<div className="active" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/men">Men</a>
-                            {props.match.params.menu == 'men' ? (<div className="active" id="men" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/shoes">Shoes</a>
-                            {props.match.params.menu == 'shoes' ? (<div className="active" id="shoes" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/bags">Bags</a>
-                            {props.match.params.menu == 'bags' ? (<div className="active" id="bags" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/accessories">Accessories</a>
-                            {props.match.params.menu == 'accessories' ? (<div className="active" id="accessories" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/beauty">Beauty</a>
-                            {props.match.params.menu == 'beauty' ? (<div className="active" id="beauty" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/kids">Kids</a>
-                            {props.match.params.menu == 'kids' ? (<div className="active" id="kids" />) : ''}
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products/on-sale">On Sale</a>
-                            {props.match.params.menu == 'on-sale' ? (<div className="active" id="on-sale" />) : ''}
-                        </li> */}
+                        
                     </ul>
                 </div>
             </div>
             
         </nav>
+        <div class="modal fade" id="modalLogin" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Welcome To Bazar</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form>
+      <div class="form-group">
+         <input type="username" class="form-control" id="username" placeholder="username"/>
+      </div>
+      <div class="form-group">
+    <input type="password" class="form-control" id="pwd" placeholder="Password"/>
+    </div>
+    <div class="checkbox">
+    <label><input type="checkbox"/> Remember me</label>
+    <br>
+    </br>
+    <a href="#">Forgot Password?</a>
+    </div>
+        </form>
+       
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dark">Sign In</button>
+        </div>
+        <div>
+            <br></br>
+     <a href="#">Forgot Password</a> | <a href="#">Sign Up</a>
+     </div>
+     <div class="auth">or sign in with</div>
+     <button type="button" class="btn btn-primary"> <img src={Facebook} alt="Facebook" />Facebook</button>
+     <button type="button" class="btn "> <img src={Google} alt="Google" />Google</button>
+    </div>
+  </div>
+</div>
+
+        
+      </>
     )
 }
 
