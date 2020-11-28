d1888181c6be34a9225ab29319c021907f14cbd7
falihnaufal17 <dudu.kotkot@gmail.com>
2020-11-28 22:04:57 +0700

    feat: add placeholder loader in product detail

---
+++ src/components/ProductDetail/index.js
@@ -0,0 +1,223 @@
+import React, { useState, useEffect, useRef, lazy } from 'react';
+import '../../assets/styles/ProductDetail.scss';
+import Slider from 'react-slick';
+import { handleChange } from '../../helpers';
+
+const ProductDetailComponent = (props) => {
+    let token = localStorage.getItem('token');
+    const [nav1, setNav1] = useState(null);
+    const [nav2, setNav2] = useState(null);
+    const [modals, setModals] = useState(0);
+    let slider1 = useRef(null);
+    let slider2 = useRef(null);
+    let modalCart = useRef(null);
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        setNav1(slider1)
+        setNav2(slider2)
+        props.fetchProductDetail(props.apiUrl, props.match.params.product_id);
+        if(JSON.parse(localStorage.getItem('profile')) != null){
+            setUserId(JSON.parse(localStorage.getItem('profile')).id)
+        }
+    }, [])
+
+    const [data, setData] = useState({
+        color: '',
+        listing_id: props.product.id,
+        size: '',
+        quantity: 1,
+        user_id: userId
+    });
+
+    const addToCart = () => {
+        if(token != null){
+            // hit api add to cart
+        }else{
+            window.location.href = '/login'
+        }
+    }
+
+    let activeClass = "cp-placeholder transparent";
+    if(props.isLoading){
+        activeClass = activeClass + " active"
+    }
+
+    return (
+        <>
+            <section id="detail" className={activeClass}>
+                <div ref={modalCart} class="modal fade modal-cart" id="addToCart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div className="position-relative image-cart">
+                                    <img src={props.product.images[0].path} className="w-100" alt={props.product.images[0].path} />
+                                </div>
+                                <div className="text-center">
+                                    <div>1 Item | Size {data.size}</div>
+                                    <div>{props.product.title}</div>
+                                    <div className="mb-3">Total $ {props.product.sizes[0].price}</div>
+                                    <a href="/bags" className="btn-primary mt-3">VIEW BAG & CHECKOUT</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">{modals}</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-12 col-lg-6">
+                            <div className="row">
+                                <div className="col-12 col-md-10">
+                                    <Slider
+                                        asNavFor={nav2}
+                                        ref={slider => { slider1 = slider }}
+                                    >
+                                        {
+                                            props.product.images.map((item, index) => {
+                                                return (
+                                                    <div className="image obj-el">
+                                                        <img src={item.path} id={item.image_id} alt={`item-${item.image_id}`} type="button" data-toggle="modal" data-target="#exampleModal" onClick={() => setModals(<img src={item.path} id={index} alt={`item-${item.image_id}`} />)} />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </Slider>
+                                </div>
+                                <div className="col-12 col-md-2">
+                                    <Slider
+                                        asNavFor={nav1}
+                                        ref={slider => (slider2 = slider)}
+                                        slidesToShow={3}
+                                        swipeToSlide={true}
+                                        focusOnSelect={true}
+                                        vertical={true}
+                                        verticalSwiping={true}
+                                        centerMode={true}
+                                        infinite={true}
+                                        centerPadding={"60px"}
+                                        responsive={
+                                            [
+                                                {
+                                                    breakpoint: 600,
+                                                    settings: {
+                                                        dots: false,
+                                                        infinite: true,
+                                                        speed: 500,
+                                                        slidesToShow: 3,
+                                                        slidesToScroll: 1,
+                                                        variableWidth: false,
+                                                        centerMode: false,
+                                                        vertical: false,
+                                                        verticalSwiping: false
+                                                    }
+                                                },
+                                                {
+                                                    breakpoint: 480,
+                                                    settings: {
+                                                        infinite: false,
+                                                        speed: 500,
+                                                        dots: false,
+                                                        slidesToShow: 3,
+                                                        slidesToScroll: 1,
+                                                        centerMode: true,
+                                                        vertical: false,
+                                                        verticalSwiping: false
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    >
+                                        {
+                                            props.product.images.map((item, index) => {
+                                                return (
+                                                    <div className="image obj-el">
+                                                        <img src={item.path} id={item.image_id} alt={`item-${item.image_id}`} />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </Slider>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-12 col-lg-6">
+                            <h1 className="product-name"><span className="obj-el">{props.product.title}</span></h1>
+                            <h2 className="price"><span className="obj-el">$ {props.product.sizes[0].price}</span></h2>
+                            <div className="form-group">
+                                <label className="label"><span className="obj-el">Color</span></label>
+                                <div className="flex-container">
+                                    {
+                                        props.product.images.map((d, i) => {
+                                            return (
+                                                <div className="flex-items color" key={i}>
+                                                    <div className="item-color obj-el" onClick={() => setData({ ...data,color: d.type })} style={{ backgroundColor: d.type }} />
+                                                    <label className="label text-center"><span className="obj-el">{d.type}</span></label>
+                                                    {
+                                                        data.color == d.type ?
+                                                            (
+                                                                <div className="item-active" />
+                                                            ) :
+                                                            (
+                                                                <div />
+                                                            )
+                                                    }
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12 col-md">
+                                    <div className="form-group">
+                                        <label className="label"><span className="obj-el">Size</span></label>
+                                        <div className="input-group obj-el">
+                                            <select className="form-control select" name="size" onChange={(e) => handleChange(e, data, setData)}>
+                                                {
+                                                    props.product.sizes.map((d, i) => {
+                                                        return (
+                                                            <option value={d.size}>{d.size}</option>
+                                                        )
+                                                    })
+                                                }
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <label className="label"><span className="obj-el">About The Brand</span></label>
+                            <p className="description obj-el" dangerouslySetInnerHTML={{__html: props.product.description}} />
+                            <div className="row row-p">
+                                <div className="col col-md-auto obj-el">
+                                    <a href="#" onClick={(e) => {e.preventDefault(); addToCart()}} data-target="#addToCart" data-toggle="modal" className="btn-primary"><span className="obj-el">ADD TO CART</span></a>
+                                </div>
+                                <div className="col-auto obj-el">
+                                    <a href="#" className="btn-secondary"><span className="obj-el">FAVOURITE</span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default ProductDetailComponent;
--- src/components/placeholders/AppPlaceholder.js
+++ src/components/placeholders/AppPlaceholder.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import Favorite from '../../assets/icons/HEART SOLID 2.svg';
 import FavoriteActive from '../../assets/icons/FAVORITE ACTIVE.svg';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import imgProduct from '../../assets/images/adidas-originals-black-3-stripes-t-shirt 1.svg';
-import ProductItem from '../ProductItem'
-import SideBarProducts from '../SideBarProducts'
 import ProductsComponent from '../Products';
+import Slider from 'react-slick';
+import '../../assets/styles/ProductDetail.scss';
 export const ProductItemLoader = (props) => {
     let result = [
         {
@@ -103,5 +101,175 @@ export const ProductItemLoader = (props) => {
 export const ProductsPlaceholder = (props) => {
     return(
         <ProductsComponent {...props} />
+    )
+}
+
+export const ProductDetailPlaceholder = (props) => {
+    let product = {
+        images: [
+            {
+                image_id: 1,
+                path: "https://via.placeholder.com/150",
+                type: 'Lorem'
+            },
+            {
+                image_id: 2,
+                path: "https://via.placeholder.com/150",
+                type: 'Lorem'
+            },
+            {
+                image_id: 3,
+                path: "https://via.placeholder.com/150",
+                type: 'Lorem'
+            },
+            {
+                image_id: 4,
+                path: "https://via.placeholder.com/150",
+                type: 'Lorem'
+            },
+            {
+                image_id: 5,
+                path: "https://via.placeholder.com/150",
+                type: 'Lorem'
+            }
+        ],
+        title: 'Lorem Ipsum Dolor Sit Amet',
+        sizes: [
+            {
+                size: 'lorem',
+                price: 100
+            }
+        ],
+        description: "<ul><li class=cu>With an adjustable ankle strap and single vamp strap, the Sam Edelman® Patti sandals are a sexy shoe choice that'll take an outfit from ho-hum to va-va-voom.</li><li class=fu>SKU: #<!-- -->8674246</li><li>Heeled sandal is available in a variety of leather, suede, man-made, and textile uppers. </li><li>Fit technology features a padded sock to boost comfort.</li><li>Synthetic insole. </li><li>Wrapped stem heel.</li><li>Leather or man-made outsole.</li><li>Imported.</li><li>Product measurements were taken using size 6, width M. Please note that measurements may vary by size.</li><li>Weight of footwear is based on a single item, not a pair.</li><li>Measurements:<ul>    <li> Heel Height: 3 in</li>    <li> Weight: 4 oz</li>    </ul></li></ul>"
+    }
+
+    let activeClass = "cp-placeholder transparent";
+    if(props.isLoading){
+        activeClass = activeClass + " active"
+    }
+
+    return(
+        <div className={"container " + activeClass}>
+            <div className="row">
+                <div className="col-12 col-md-12 col-lg-6">
+                    <div className="row">
+                        <div className="col-12 col-md-10">
+                            <Slider
+                            >
+                                {
+                                    product.images.map((item, index) => {
+                                        return (
+                                            <div className="image obj-el">
+                                                <img src={item.path} id={item.image_id} alt={`item-${item.image_id}`} type="button" data-toggle="modal" data-target="#exampleModal" />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </Slider>
+                        </div>
+                        <div className="col-12 col-md-2">
+                            <Slider
+                                slidesToShow={3}
+                                swipeToSlide={true}
+                                focusOnSelect={true}
+                                vertical={true}
+                                verticalSwiping={true}
+                                centerMode={true}
+                                infinite={true}
+                                centerPadding={"60px"}
+                                responsive={
+                                    [
+                                        {
+                                            breakpoint: 600,
+                                            settings: {
+                                                dots: false,
+                                                infinite: true,
+                                                speed: 500,
+                                                slidesToShow: 3,
+                                                slidesToScroll: 1,
+                                                variableWidth: false,
+                                                centerMode: false,
+                                                vertical: false,
+                                                verticalSwiping: false
+                                            }
+                                        },
+                                        {
+                                            breakpoint: 480,
+                                            settings: {
+                                                infinite: false,
+                                                speed: 500,
+                                                dots: false,
+                                                slidesToShow: 3,
+                                                slidesToScroll: 1,
+                                                centerMode: true,
+                                                vertical: false,
+                                                verticalSwiping: false
+                                            }
+                                        }
+                                    ]
+                                }
+                            >
+                                {
+                                    product.images.map((item, index) => {
+                                        return (
+                                            <div className="image obj-el">
+                                                <img src={item.path} id={item.image_id} alt={`item-${item.image_id}`} />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </Slider>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-12 col-lg-6">
+                    <h1 className="product-name"><span className="obj-el">{product.title}</span></h1>
+                    <h2 className="price"><span className="obj-el">$ {product.sizes[0].price}</span></h2>
+                    <div className="form-group">
+                        <label className="label"><span className="obj-el">Color</span></label>
+                        <div className="flex-container">
+                            {
+                                product.images.map((d, i) => {
+                                    return (
+                                        <div className="flex-items color" key={i}>
+                                            <div className="item-color obj-el" style={{ backgroundColor: d.type }} />
+                                            <label className="label text-center"><span className="obj-el">{d.type}</span></label>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md">
+                            <div className="form-group">
+                                <label className="label"><span className="obj-el">Size</span></label>
+                                <div className="input-group obj-el">
+                                    <select className="form-control select" name="size">
+                                        {
+                                            product.sizes.map((d, i) => {
+                                                return (
+                                                    <option value={d.size}>{d.size}</option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <label className="label"><span className="obj-el">About The Brand</span></label>
+                    <p className="description obj-el" dangerouslySetInnerHTML={{__html: product.description}} />
+                    <div className="row row-p">
+                        <div className="col col-md-auto obj-el">
+                            <a href="#" data-target="#addToCart" data-toggle="modal" className="btn-primary"><span className="obj-el">ADD TO CART</span></a>
+                        </div>
+                        <div className="col-auto obj-el">
+                            <a href="#" className="btn-secondary"><span className="obj-el">FAVOURITE</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
